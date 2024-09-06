909098435e7ef3da0b61ce2d646a9c6ae921aedf
lukew <lukew@crynux.ai>
2024-09-07 00:01:33 +0800

    [feature] similarity score

--- genki/validator/model_evaluator/music/scores/apis/similarity.py
+++ genki/validator/model_evaluator/music/scores/apis/similarity.py
@@ -1,3 +1,4 @@
+from flask import current_app
 
-def get_similarity_score(text, num_claps):
-    return ""
+def get_similarity_score(folder1, folder2):
+    return current_app.config["frechet"].score(folder1, folder2)
--- genki/validator/model_evaluator/music/scores/app.py
+++ genki/validator/model_evaluator/music/scores/app.py
@@ -1,19 +1,25 @@
 from flask import Flask, request, jsonify
 from apis.clap import get_clap_score
 from apis.similarity import get_similarity_score
-from frechet_audio_distance import CLAPScore
+from frechet_audio_distance import CLAPScore, FrechetAudioDistance
 
 from pathlib import Path
 
+
 app = Flask(__name__)
 
-def init_models():
 
+def init_models():
     model_cache_dir = Path(__file__).parent / "models"
 
     app.config['clap'] = CLAPScore(
         ckpt_dir=model_cache_dir
     )
+
+    app.config['frechet'] = FrechetAudioDistance(
+        ckpt_dir=model_cache_dir
+    )
+
 
 @app.route('/clap', methods=['GET'])
 def clap():
@@ -23,12 +29,14 @@ def clap():
     score_mean, score_std = get_clap_score(csv_file, music_folder, text_column)
     return jsonify({"mean": float(score_mean), "std": float(score_std)})
 
+
 @app.route('/similarity', methods=['GET'])
 def similarity():
-    text1 = request.args.get('text1', '')
-    text2 = request.args.get('text2', '')
-    result = get_similarity_score(text1, text2)
-    return jsonify({"result": result})
+    folder1 = request.args.get('music_folder_a', '')
+    folder2 = request.args.get('music_folder_b', '')
+    result = get_similarity_score(folder1, folder2)
+    return jsonify({"similarity": float(result)})
+
 
 if __name__ == "__main__":
     init_models()
